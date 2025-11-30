348147199492a6b82d51bcf078f564aa0235b77f
MACBOOK <macbook@192.168.1.7>
2025-11-30 22:10:51 +0100

    fourth commit

--- app/Http/Controllers/User/DepositController.php
+++ app/Http/Controllers/User/DepositController.php
@@ -7,6 +7,7 @@ use Illuminate\Http\Request;
 use Illuminate\Validation\Rules\File;
 use App\Models\Deposit;
 use App\Models\Account;
+use App\Models\Transaction;
 
 class DepositController extends Controller
 {
@@ -41,9 +42,30 @@ class DepositController extends Controller
                 'deposit_address' => $requestData['deposit_address'],
                 'deposit_proof' => $requestData['deposit_proof']
             ]);
+            $account = Account::findOrFail($account_id);
 
-            $account = Account::where('user_id', $user->id)->update([
-                'balance' => $requestData['amount']
+            $account->update([
+                'balance' => $account->balance + $requestData['amount']
+            ]);
+
+            // $account = Account::where('user_id', $user->id)->update([
+            //     'balance' => $requestData['amount']
+            // ]);
+
+            // Transaction
+            $reference = 'DEP-' . substr(rand(0, time()), 0, 7);
+            $description = 'Deposited: ' . '₦' . $requestData['amount'];
+            $transaction = Transaction::create([
+                'account_id' => $account_id,
+                'reference' => $reference,
+                'type' => 'debit',
+                'amount' => $requestData['amount'],
+                'balance_before' => $account->balance + $requestData['amount'],
+                'balance_after' => $account->balance - $requestData['amount'],
+                'category' => 'transfer',
+                'description' => $description,
+                'source_account' => $account->account_number,
+                'destination_account' => $requestData['deposit_address']
             ]);
         } catch (\Exception $e) {
             return back()->with('error', 'There was an error depositing your account: ' . $e->getMessage());
--- resources/views/user/signup.blade.php
+++ resources/views/user/signup.blade.php
@@ -219,7 +219,7 @@
                                         @enderror
                                     </div>
                                     <div class="mb-4">
-                                        <input type="text" name="phone"
+                                        <input type="tel" name="phone"
                                             class="placeholder:text-gray-500 text-sm focus:shadow-primary-outline leading-5.6 ease block w-full appearance-none rounded-lg border border-solid border-gray-300 bg-white bg-clip-padding py-2 px-3 font-normal text-gray-700 transition-all focus:border-blue-500 focus:bg-white focus:text-gray-700 focus:outline-none focus:transition-shadow"
                                             placeholder="Phone Number" aria-label="Phone Number"
                                             aria-describedby="phone-addon" required />
